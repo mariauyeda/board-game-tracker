7687f1dee8e6428c38067f03a906084ef723954e
soctkoren <joimkim@gmail.com>
2016-09-15 10:04:47 -0700

    add all controllers, resource and devise user views

--- boardgametracker/config/routes.rb
+++ boardgametracker/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
+
+  resources :games
+  resources :tags
+  resources :comments
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'game#index'
 end
