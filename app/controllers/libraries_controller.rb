4a79b29e4e71f659b04fa7d9194f52a5bda298d7
Ryker <ryker.reed3@gmail.com>
2016-09-16 16:08:57 -0700

    Allows user to add games to their library.

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,5 +1,20 @@
 class LibrariesController < ApplicationController
-    def index
+  def index
     #query current_user and all friends
   end
+
+  def new
+  	@games = Game.all
+  	@library = Library.new
+  end
+
+  def create
+	game_id = params[:library][:game_id]
+	user_id = current_user.id
+	@library = Library.new({:user_id => user_id, :game_id => game_id})
+	if @library.save
+		redirect_to user_path(current_user)
+	end
+  end
+
 end
